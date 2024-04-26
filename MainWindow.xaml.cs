85d2d13df5b20c4677700958492b19c3fd810dee
Connor J Thompson <ct770a@gmail.com>
2024-04-27 01:47:14 +1000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jtrack
+{
+    // UI for accepting urls
+    // switch case depending on url
+    // differe+nt instructions for finding title, desc, & other details
+    // inspect and manage listings
+    // 
+
+
+
+
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
